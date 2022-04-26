12524071fcc3bb57dc20f6e972b93506f7b4a594
1iwnl1 <83950719+1iwnl1@users.noreply.github.com>
2022-04-27 01:38:56 +0300

    v1

---
+++ js/main.js
@@ -0,0 +1,45 @@
+var divabout = document.getElementById("divabout");
+
+function addTask () {
+    var input = document.getElementById("input");
+    
+    // get current text from input field
+    var newTask = input.value;
+    // only add new item to list if some text was entered 
+    if (newTask != "") {
+        // create new HTML list item
+        var item = document.createElement("li");
+        // add HTML for buttons and new task text
+        // Note, need to use '' because of "" in HTML
+        item.innerHTML = '<input type="button" class="done" onclick="markDone(this.parentNode)"  value="&#x2713;" /> ' + '<input type="button" class="remove" onclick="remove(this.parentNode)" value="&#x2715;" /> ' + newTask;
+
+        // add new item as part of existing list
+        document.getElementById("tasks").appendChild(item); 
+
+        input.value = "";
+        input.placeholder = "enter next task …";
+    }
+}
+
+// change styling used for given item
+function markDone (item) {
+    item.className = 'finished';
+}
+
+/* Step 7 below here */
+function remove (item) {
+    if (item.className == 'finished'){
+        item.remove();
+    }
+}
+
+/* Step 11 below here */
+function doAbout() {
+    divabout.textContent = "Author is Dmyro Alieksieiev";
+}
+
+/* Step 14 below here */
+function clearAbout() {
+    divabout.textContent = "";
+}
+
